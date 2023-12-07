f8e911407344478639026ec2001883155fee0671
RaphaelIT7 <raphael.i.t@outlook.de>
2023-12-07 19:51:31 +0100

    Fixed sv_parallel_* crashing.

--- engine/framesnapshot.h
+++ engine/framesnapshot.h
@@ -166,8 +166,9 @@ public:
 private:
 	void	DeleteFrameSnapshot( CFrameSnapshot* pSnapshot );
 
-	CUtlLinkedList<CFrameSnapshot*, int>		m_FrameSnapshots;
-	CClassMemoryPool< PackedEntity >			m_PackedEntitiesPool;
+	CThreadFastMutex									m_FrameSnapshotsWriteMutex;
+	CUtlLinkedList<CFrameSnapshot*, unsigned short>		m_FrameSnapshots;
+	CClassMemoryPool< PackedEntity >					m_PackedEntitiesPool;
 
 	int								m_nPackedEntityCacheCounter;  // increase with every cache access
 	CUtlVector<UnpackedDataCache_t>	m_PackedEntityCache;	// cache for uncompressed packed entities
